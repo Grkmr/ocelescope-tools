f48b8ca3fdaf45a31392f9baba3256a9047c8f8d
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-08-02 15:44:25 +0200

    :tada: First commit

---
+++ src/ocelescope_tools_grkmr/ocel/filter/__init__.py
@@ -0,0 +1,79 @@
+from typing import Optional, TypedDict, cast
+
+import pandas as pd
+import pm4py
+
+from ocel.ocel import OCEL
+
+from .attribute import EventAttributeFilter, ObjectAttributeFilter
+from .base import BaseFilter, FilterResult
+from .entity_type import (
+    EventTypeFilter,
+    ObjectTypeFilter,
+)
+from .relation_count import E2OCountFilter, O2OCountFilter
+from .time_range import TimeFrameFilter
+
+
+class OCELFilter(TypedDict):
+    object_types: ObjectTypeFilter
+    event_type: EventTypeFilter
+    time_range: TimeFrameFilter
+    o2o_count: list[O2OCountFilter]
+    e2o_count: list[E2OCountFilter]
+    event_attributes: list[EventAttributeFilter]
+    object_attributes: list[ObjectAttributeFilter]
+
+
+def compute_combined_masks(ocel: OCEL, filters: OCELFilter) -> FilterResult:
+    combined = FilterResult(
+        events=pd.Series(True, index=ocel.events.index),
+        objects=pd.Series(True, index=ocel.objects.index),
+    )
+
+    for filter in filters.values():
+        filter_list = cast(list[BaseFilter], filter if isinstance(filter, list) else [filter])
+
+        for filter in filter_list:
+            combined = combined.and_merge(filter.filter(ocel))
+
+    return combined
+
+
+def apply_filters(ocel: OCEL, filters: OCELFilter) -> OCEL:
+    masks = compute_combined_masks(ocel, filters)
+
+    filtered_event_ids: Optional[pd.Series] = (
+        cast(pd.Series, ocel.events[ocel.ocel.event_id_column][masks.events])
+        if masks.events is not None
+        else None
+    )
+
+    filtered_object_ids: Optional[pd.Series] = (
+        cast(pd.Series, ocel.objects[ocel.ocel.object_id_column][masks.objects])
+        if masks.objects is not None
+        else None
+    )
+
+    filtered_ocel = ocel.ocel
+
+    if filtered_event_ids is not None:
+        filtered_ocel = pm4py.filter_ocel_events(filtered_ocel, filtered_event_ids, positive=True)
+
+    if filtered_object_ids is not None:
+        filtered_ocel = pm4py.filter_ocel_objects(filtered_ocel, filtered_object_ids, positive=True)
+
+    return OCEL(filtered_ocel, ocel.id)
+
+
+__all__ = [
+    "apply_filters",
+    "OCELFilter",
+    "ObjectTypeFilter",
+    "EventTypeFilter",
+    "ObjectAttributeFilter",
+    "EventAttributeFilter",
+    "O2OCountFilter",
+    "E2OCountFilter",
+    "TimeFrameFilter",
+]
---
+++ src/ocelescope_tools_grkmr/ocel/filter/base.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Optional
+
+from pandas import Series
+from pydantic import BaseModel
+
+from ocel.ocel import OCEL
+
+
+@dataclass()
+class FilterResult:
+    events: Optional[Series] = None
+    objects: Optional[Series] = None
+
+    def and_merge(self, other: "FilterResult") -> "FilterResult":
+        def _and(a, b):
+            if a is not None and b is not None:
+                return a & b
+            elif a is not None:
+                return a
+            elif b is not None:
+                return b
+            else:
+                return None
+
+        return FilterResult(
+            events=_and(self.events, other.events),
+            objects=_and(self.objects, other.objects),
+        )
+
+
+class BaseFilter(ABC, BaseModel):
+    @abstractmethod
+    def filter(self, ocel: OCEL) -> FilterResult:
+        pass
